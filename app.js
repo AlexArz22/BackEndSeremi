872e37e91815956aeefae05174828969cb3b1004
lostintothevoid <127423332+lostintothevoid@users.noreply.github.com>
2025-06-05 20:48:10 -0400

    establecio coneccion
    se ha establecido conexion con el front end

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const userRoutes = require('./routes/userRoute');
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/usuarios', userRoutes);
 config={SERVER:"localhost",PORT:3000}
